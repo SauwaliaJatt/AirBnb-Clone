d5f41d8986f6a98505e50e2bfcf536ec0f6bfc5c
Inderjit Singh <sauwaliajatt@gmail.com>
2025-08-04 19:45:59 +0530

    edit Route Updated

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+
 
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/CourseMajorDB";
 
@@ -31,12 +34,42 @@ app.get("/listings", async (req, res) => {
     res.render("listings/index", {allListings});
 });
 
+
+//New and Create route
+app.get("/listings/new", async (req, res) => {
+    res.render("listings/new");
+});
+
+app.post("/listings", async (req, res) => {
+    // const {title, description, price, location, country} = req.body;
+    //it get simper due to listing[title]..... in form as it directly become object like and we can directly access it without above method and easy to use
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+
+//Edit and Update route
+app.get("/listings/:id/edit", async (req, res) => {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", {listing});
+});
+
+app.put("/listings/:id", async (req, res) => {
+    const {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+});
+
+
 //show route
 app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show", {listing});
 });
+
 
 
 app.listen(8080, () => {
