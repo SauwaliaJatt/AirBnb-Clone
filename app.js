917c90bb828fab1d338ee7fc9eccde52e3268604
Inderjit Singh <sauwaliajatt@gmail.com>
2025-08-04 17:01:32 +0530

    Route setup

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: true}));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/CourseMajorDB";
 
@@ -17,14 +24,20 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-app.get("/", async (req, res) => {
-    try {
-        const listings = await Listing.find({});
-        res.json(listings);
-    } catch (err) {
-        res.status(500).send("Error fetching listings");
-    }
+
+//listing route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index", {allListings});
 });
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show", {listing});
+});
+
 
 app.listen(8080, () => {
     console.log("Server is listening to port 8080");
