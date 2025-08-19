30ee4063eff109b87904987bc77cb8874aa7b80c
Inderjit Singh <sauwaliajatt@gmail.com>
2025-08-19 11:46:54 +0530

    logout added

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
+const { isLoggedIn } = require("../middleware.js");
 
 
 //validate Listingsusing joi
@@ -26,12 +27,12 @@ router.get("/", wrapAsync(async (req, res) => {
 
 
 //New and Create route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new");
 });
 
 
-router.post("/", validateListing, wrapAsync (async (req, res, next) => {
+router.post("/", isLoggedIn, validateListing, wrapAsync (async (req, res, next) => {
     // const {title, description, price, location, country} = req.body;
     //it get simper due to listing[title]..... in form as it directly become object like and we can directly access it without above method and easy to use
     const newListing = new Listing(req.body.listing);
@@ -54,7 +55,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 
 
 //Edit and Update route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
     const {id} = req.params;
     const listing = await Listing.findById(id);
 
@@ -65,7 +66,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
     res.render("listings/edit", {listing});
 }));
 
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, validateListing, wrapAsync(async (req, res) => {
     const {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
@@ -75,7 +76,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 
 
 //Delete route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, wrapAsync(async (req, res) => {
     const {id} = req.params;
     await Listing.findByIdAndDelete(id);
     req.flash("success", "Listing Deleted!");
